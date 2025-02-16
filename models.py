7036b1f904ceaa5c3dc56cb7f76f4b408acc8cd3
Pulasthya Reddy S <pulasthyareddy@Pulasthyas-Air.lan>
2025-02-16 15:36:51 -0500

    table for user refresh token

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
+from datetime import datetime, timedelta
 
 class User(Base):
     __tablename__ = "users"
@@ -21,3 +22,15 @@ class UserDetails(Base):
     role = Column(String)
 
     user = relationship("User", back_populates="details")
+
+class UserSession(Base):
+    __tablename__ = "user_sessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey("users.email"))
+    refresh_token = Column(String, unique=True)
+    device_info = Column(String)
+    last_used = Column(DateTime, default=datetime.utcnow)
+    expires_at = Column(DateTime)
+
+    user = relationship("User", back_populates="sessions")
